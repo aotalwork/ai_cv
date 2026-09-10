6afc0a7379039979a42b539e1c1c6d87ebbbbddd
aotalwork <aotalwork@gmail.com>
2026-09-10 11:17:48 +0200

    Commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @profile = Profile.first
+
+    @experiences = @profile.experiences.order(start_date: :desc)
+    @skills = @profile.skills.order(:category, :name)
+    @certifications = @profile.certifications.order(year: :desc)
+    @educations = @profile.educations.order(start_year: :desc)
+    @languages = @profile.languages.order(:name)
+    @achievements = @profile.achievements.order(:position)
+    @recommendations = @profile.recommendations.order(date: :desc)
   end
 end
